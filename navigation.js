e7f8bdc1fd0005a7712a504b9a5864fc86d041b9
Maniac0001 <91536265+thrift-floor@users.noreply.github.com>
2025-01-04 23:17:48 +0530

    ssssss

---
+++ navigation.js
@@ -0,0 +1,7 @@
+document.getElementById('about-card').addEventListener('click', function() {
+    window.location.href = 'about.html'; // Navigate to about.html
+});
+
+document.getElementById('contact-card').addEventListener('click', function() {
+    window.location.href = 'contact.html'; // Navigate to contact.html
+});
